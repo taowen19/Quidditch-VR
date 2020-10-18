3eed2683554a72e133342362f03c1fd4a22382ab
Taowen Liu <282620719@qq.com>
2020-10-17 20:51:43 -0700

    Add files via upload

---
+++ Assets/chase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chase : MonoBehaviour {
+	float frame = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (frame < 200) {
+			Vector3 dir = new Vector3 (1.3f, 1.0f, 1.1f);
+			float scalor = 0.09f + Time.deltaTime;
+			transform.Translate (dir * scalor);
+
+		} else if (200 <= frame && frame < 300) {
+			Vector3 dir = new Vector3 (-1.5f, -1.0f, 1.0f);
+			float scalor = 0.09f + Time.deltaTime;
+			transform.Translate (dir * scalor);
+
+		} else if (300 <= frame && frame < 500) {
+			transform.rotation = Quaternion.Euler (0.0f, -90.0f, 0.0f);
+			Vector3 dir = new Vector3 (-1.0f, 0.0f, 1.0f);
+			float scalor = 0.09f + Time.deltaTime;
+			transform.Translate (dir * scalor);
+		} else if (500 <= frame && frame < 600) {
+			Vector3 dir = new Vector3 (-1.0f, 1.0f, 1.0f);
+			float scalor = 0.15f + Time.deltaTime;
+			transform.Translate (dir * scalor);
+		} else {
+		}
+			frame = frame + 1;
+
+	}
+}
---
+++ Assets/fly.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fly : MonoBehaviour {
+	bool isBroomActive;
+
+	// Use this for initialization"
+	void Start () {
+
+		isBroomActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (GameObject.Find ("Broom") != null) {
+
+			Vector3 direction = Camera.main.transform.forward;
+			float scalor = 0.1f + Time.deltaTime;
+			transform.Translate (direction * scalor);
+			if(GameObject.Find ("hopOn") != null){
+				GameObject.Find ("hopOn").SetActive (false);
+			}
+		}
+		//如果broom appear， then move, and set hopOn as false
+	}
+}
